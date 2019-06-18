34a4804bed633d676316d0bf348c00b73352f1e3
Valery Kulikov <frostyland@gmail.com>
2019-06-18 18:37:22 +0300

    again

--- main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:angular/angular.dart';
-import 'package:angular_tour_of_heroes/app_component.template.dart' as ng;
-import 'package:pwa/client.dart' as pwa;
-
-void main() {
-  // register PWA ServiceWorker for offline caching.
-  pwa.Client();
-  runApp(ng.AppComponentNgFactory);
-}
