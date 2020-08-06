71a3de61620e8c3a3baecd3197b5d44d23258748
vvk@work <frostyland@gmail.com>
2020-08-06 16:34:34 +0500

    do checking flag

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,7 +11,7 @@ const RESOURCES = {
 "index.html": "28fbab68e057170f278cc241120723ac",
 "/": "28fbab68e057170f278cc241120723ac",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "56eb1aab9bbe2a416e28ba3e2296d62f",
+"main.dart.js": "b844fc806589106aae38b27b472f136f",
 "manifest.json": "1df6170abfa056ff7baa7da1100bae3f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
