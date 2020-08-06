304c6e0582e07d3be13bd414655d817722443d09
vvk@work <frostyland@gmail.com>
2020-08-06 14:33:55 +0500

    added list

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,7 +11,7 @@ const RESOURCES = {
 "index.html": "28fbab68e057170f278cc241120723ac",
 "/": "28fbab68e057170f278cc241120723ac",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "28de0f74b44f8ad310a11ae4b83c81f2",
+"main.dart.js": "fa42337287aeb349cc42800c7c710aa7",
 "manifest.json": "1df6170abfa056ff7baa7da1100bae3f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
