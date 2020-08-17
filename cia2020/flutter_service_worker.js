a5897b31544517b661cbfe3f83f77c6be9a4326e
vvk@work <frostyland@gmail.com>
2020-08-17 12:09:54 +0500

    + profile list

--- cia2020/flutter_service_worker.js
+++ cia2020/flutter_service_worker.js
@@ -17,7 +17,7 @@ const RESOURCES = {
 "index.html": "e2e607d6aecc28eb13f28bc4179e5635",
 "/": "e2e607d6aecc28eb13f28bc4179e5635",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "eb9a8e6ab8afe6fd8811d980c068ae3d",
+"main.dart.js": "790bcd5b6f2b4544e48f9775e494df62",
 "manifest.json": "7ea128407400134c5e09cf2511b0e15f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
